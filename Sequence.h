804f48766522c415c402d5fa6e17c5b42d5ebd6d
Jack Dolbeer <jack.dolbeer123@gmail.com>
2025-10-14 23:14:27 -0400

    Sequence.h Update #1

---
+++ Sequence.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+class Sequence {
+
+    private:
+    std::string* data;
+    size_t numElements;
+
+    public:
+    Sequence(size_t sz = 0);
+    Sequence(const Sequence& s);
+    ~Sequence();
+
+    Sequence& operator=(const Sequence& s);
+    std::string& operator[](size_t position);
+
+    void push_back(std::string item);
+    void pop_back();
+    void insert(size_t position, std::string item);
+    std::string front() const;
+    std:: string back() const;
+    bool empty() const;
+    size_t size() const;
+    void clear();
+    void erase(size_t position);
+    void erase(size_t position, size_t count);
+    friend std::ostream& operator<<(std::ostream& os, const Sequence& s);
+};
